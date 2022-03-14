9a19ae02f023951bd3f1cd07f36d1dc2cfd3eaa8
Shang-QY <55442231+Shang-QY@users.noreply.github.com>
2022-03-14 16:39:34 +0800

    init ebookstore
    Signed-off-by: Shang-QY <55442231+Shang-QY@users.noreply.github.com>

---
+++ app/src/main/java/com/example/ebookstore/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,50 @@
+package com.example.ebookstore.ui.notifications;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.ebookstore.MainActivity;
+import com.example.ebookstore.R;
+import com.example.ebookstore.databinding.FragmentNotificationsBinding;
+import com.example.ebookstore.ui.login.LoginActivity;
+
+public class NotificationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        root.findViewById(R.id.bn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
